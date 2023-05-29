4d12f7a88e9b444219e56040981d2ec93589cd6e
rira <101010hayakawa@gmail.com>
2023-05-29 23:23:30 +0900

    スコープで指定する対象をuser_idからuserに変更

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -16,7 +16,7 @@ class Blog < ApplicationRecord
   scope :default_order, -> { order(id: :desc) }
 
   scope :viewable, lambda { |user|
-    where(secret: false).or(where(user_id: user&.id))
+    where(secret: false).or(where(user: user))
   }
 
   def owned_by?(target_user)
